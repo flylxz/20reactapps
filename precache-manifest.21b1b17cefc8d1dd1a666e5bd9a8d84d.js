4d63a924672acfc59969acfa986601f62afbef27
flylxz <flylxz@gmail.com>
2020-10-22 00:46:33 +0300

    Updates

---
+++ precache-manifest.21b1b17cefc8d1dd1a666e5bd9a8d84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feefd8a13067d42f7408471c5d400b02",
+    "url": "/20reactapps/index.html"
+  },
+  {
+    "revision": "af8830ac8ac4e4dbcc67",
+    "url": "/20reactapps/static/css/main.4242f4f5.chunk.css"
+  },
+  {
+    "revision": "f7c3791c84b5caa15f39",
+    "url": "/20reactapps/static/js/2.5d276fc5.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/20reactapps/static/js/2.5d276fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8830ac8ac4e4dbcc67",
+    "url": "/20reactapps/static/js/main.ef0cd003.chunk.js"
+  },
+  {
+    "revision": "c7addd5267c847d5916e",
+    "url": "/20reactapps/static/js/runtime-main.b9807962.js"
+  }
+]);
